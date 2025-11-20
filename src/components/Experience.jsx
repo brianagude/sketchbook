93dffb9f25360d071fecf38e96292d82ca6a608a
Briana Gude <briana.gude@gmail.com>
2025-11-19 20:07:02 -0500

    first push bestie

---
+++ src/components/Experience.jsx
@@ -0,0 +1,22 @@
+import { Physics } from "@react-three/rapier";
+import useGame from "@/stores/useGame.jsx";
+import { Level } from "./Level.jsx";
+import Lights from "./Lights.jsx";
+import Player from "./Player.jsx";
+
+export default function Experience() {
+  const blocksCount = useGame((state) => state.blocksCount);
+  const blocksSeed = useGame((state) => state.blocksSeed);
+
+  return (
+    <>
+      <color args={["#bdedfc"]} attach="background" />
+
+      <Physics debug={false}>
+        <Lights />
+        <Level count={blocksCount} seed={blocksSeed} />
+        <Player />
+      </Physics>
+    </>
+  );
+}
